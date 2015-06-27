fbe2ba5aafff6feda91f38a9d8688ed13116c465
saket <saket.j14@gmail.com>
2015-06-27 08:14:58 +0530

    saket8

--- musicalguru/settings.py
+++ musicalguru/settings.py
@@ -113,7 +113,6 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
